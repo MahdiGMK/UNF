10bbc94c788344d8e95d89ba88727ed49790a164
MBDCG <30409029+MBDCG@users.noreply.github.com>
2018-03-28 13:44:44 +0430

    solved a little mistake

--- GraphMeshTest/Assets/Graphmesh-master/Scripts/GraphmeshGraph.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using XNode;
-
-[CreateAssetMenu(menuName = "Graphmesh Graph", fileName = "New Graphmesh Graph")]
-public class GraphmeshGraph : NodeGraph {
-    
-}
